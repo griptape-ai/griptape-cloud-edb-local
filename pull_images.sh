9e23d7226dffd5f3d78a022ac852d8186d793b04
matt <matt@griptape.ai>
2025-04-09 16:05:27 -0500

    Update images and environment variables

--- pull_images.sh
+++ pull_images.sh
@@ -7,15 +7,15 @@ source .env
 docker login gtcappregistryedb.azurecr.io -u $AZURE_OPENAI_CLIENT_ID --password-stdin <<< $AZURE_OPENAI_CLIENT_SECRET
 
 # pull images from gtcappregistryedb.azurecr.io
-docker pull gtcappregistryedb.azurecr.io/griptape-release:latest
-docker pull gtcappregistryedb.azurecr.io/griptape-scheduler:latest
-docker pull gtcappregistryedb.azurecr.io/griptape-workers:latest
-docker pull gtcappregistryedb.azurecr.io/griptape-web:latest
-docker pull gtcappregistryedb.azurecr.io/griptape-runtime-event-listener:latest
+docker pull gtcappregistryedb.azurecr.io/griptape-edb/release:latest
+docker pull gtcappregistryedb.azurecr.io/griptape-edb/scheduler:latest
+docker pull gtcappregistryedb.azurecr.io/griptape-edb/workers:latest
+docker pull gtcappregistryedb.azurecr.io/griptape-edb/web:latest
+docker pull gtcappregistryedb.azurecr.io/griptape-edb/runtime-event-listener:latest
 
 # tag each one with the local registry
-docker tag gtcappregistryedb.azurecr.io/griptape-release:latest griptape-release:latest
-docker tag gtcappregistryedb.azurecr.io/griptape-scheduler:latest griptape-scheduler:latest
-docker tag gtcappregistryedb.azurecr.io/griptape-workers:latest griptape-workers:latest
-docker tag gtcappregistryedb.azurecr.io/griptape-web:latest griptape-web:latest
-docker tag gtcappregistryedb.azurecr.io/griptape-runtime-event-listener:latest griptape-runtime-event-listener:latest
+docker tag gtcappregistryedb.azurecr.io/griptape-edb/release:latest griptape-release:latest
+docker tag gtcappregistryedb.azurecr.io/griptape-edb/scheduler:latest griptape-scheduler:latest
+docker tag gtcappregistryedb.azurecr.io/griptape-edb/workers:latest griptape-workers:latest
+docker tag gtcappregistryedb.azurecr.io/griptape-edb/web:latest griptape-web:latest
+docker tag gtcappregistryedb.azurecr.io/griptape-edb/runtime-event-listener:latest griptape-runtime-event-listener:latest
